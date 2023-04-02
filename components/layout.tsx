5a87bd02e179531daaf22cb4716ec39dc6005abd
inprovert <jejurkarpranav@gmail.com>
2023-04-02 20:39:24 +0530

    lesssgooo

---
+++ components/layout.tsx
@@ -0,0 +1,18 @@
+interface LayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="mx-auto flex flex-col space-y-4">
+      <header className="container sticky top-0 z-40 bg-white">
+        
+      </header>
+      <div>
+        <main className="flex w-full flex-1 flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
